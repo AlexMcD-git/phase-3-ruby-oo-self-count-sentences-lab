abe27b41fe194372ac9ba9a293dd918ec3eff02a
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-05-18 12:51:47 -0400

    commit tree e620b4c28c13b81c234fca41ba5199097106d71a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,38 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == '.'
+      true
+    else 
+      false
+    end
   end
 
   def question?
-
+    if self[-1] == '?'
+      true
+    else 
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == '!'
+      true
+    else 
+      false
+    end
   end
 
   def count_sentences
-
+    sentences = 0
+    i=0
+    while i<self.length
+      if (self[i] == '.' || self[i] == '?' || self[i] == '!') && (self[i-1] != '.' && self[i-1] != '?' && self[i-1] != '!')
+        sentences += 1
+      end
+      i+=1
+    end
+  sentences
   end
 end
